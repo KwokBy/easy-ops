f36c56461edbcd48f90458b35e8d88d5a92081f1
doubleguo <1042936527@qq.com>
2022-03-08 12:00:04 +0800

    host repo

---
+++ repo/host.go
@@ -0,0 +1,50 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/KwokBy/easy-ops/models"
+	"gorm.io/gorm"
+)
+
+type mysqlHostRepo struct {
+	DB *gorm.DB
+}
+
+func NewMysqlHostRepo(DB *gorm.DB) IHostRepo {
+	return &mysqlHostRepo{DB}
+}
+
+// GetHostsByUsername 根据用户名获取主机列表
+func (h *mysqlHostRepo) GetHostsByUsername(ctx context.Context, username string) (
+	[]models.Host, error) {
+	var hosts []models.Host
+	if err := h.DB.Where("username = ?", username).Find(&hosts).Error; err != nil {
+		return nil, err
+	}
+	return hosts, nil
+}
+
+// AddHost 添加主机
+func (h *mysqlHostRepo) AddHost(ctx context.Context, host models.Host) error {
+	if err := h.DB.Create(&host).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteHost 删除主机
+func (h *mysqlHostRepo) DeleteHost(ctx context.Context, id int64) error {
+	if err := h.DB.Delete(&models.Host{}, id).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateHost 更新主机信息
+func (h *mysqlHostRepo) UpdateHost(ctx context.Context, host models.Host) error {
+	if err := h.DB.Save(&host).Error; err != nil {
+		return err
+	}
+	return nil
+}
